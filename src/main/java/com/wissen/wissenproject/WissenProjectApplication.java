4523ab479eeb263f1e4b9b576da02f58d3961e16
tech-stat <amar05kumar08@gmail.com>
2022-12-24 23:22:22 +0530

    skelton added

---
+++ src/main/java/com/wissen/wissenproject/WissenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wissen.wissenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WissenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WissenProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wissen/wissenproject/WissenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wissen.wissenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WissenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
